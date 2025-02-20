d178016fb765b47c29f3f37c93aa3c6857adcee1
Kusaki-Y <yuinjackson@gmail.com>
2025-02-20 12:00:45 +0800

    Add files via upload

--- src/BoosterBot/Helpers/Logger.cs
+++ src/BoosterBot/Helpers/Logger.cs
@@ -1,22 +1,27 @@
-﻿
-using System.IO;
+﻿using System.IO;
 using BoosterBot.Helpers;
 using BoosterBot.Models;
+using System.Windows.Controls;
 
 namespace BoosterBot
 {
     internal static class Logger
     {
+        // 声明一个事件或委托，UI 端可以订阅它来更新日志区域
+        public static event Action<string> OnLogUpdated;
+
         public static void Log(LocalizationManager localizer, string key, string logPath, bool directWrite = false, List<FindReplaceValue> replace = null)
         {
             var time = $"[{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}] ";
+            string logMessage = string.Empty;
+
             if (directWrite)
             {
-                Console.WriteLine(key);
+                logMessage = key;
                 if (logPath != null)
                 {
                     using var writer = new StreamWriter(logPath, true);
-                    writer.WriteLine(time + (directWrite ? key : localizer.GetNeutralString(key)));
+                    writer.WriteLine(time + key);
                 }
             }
             else
@@ -31,7 +36,7 @@ namespace BoosterBot
                         neutral = neutral.Replace(replacement.Token, replacement.Value);
                     }
 
-                Console.WriteLine(time + localized);
+                logMessage = localized;
 
                 if (logPath != null)
                 {
@@ -39,7 +44,9 @@ namespace BoosterBot
                     writer.WriteLine(time + neutral);
                 }
             }
-            
+
+            // 只发送日志到 UI
+            OnLogUpdated?.Invoke(time + logMessage);
         }
     }
 }
