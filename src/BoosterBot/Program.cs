a4c4c48c9260d66d14ca65eb0face670dc8bc67e
Rico Turman <rico.turman@littlefort.dev>
2025-11-25 17:12:41 -0600

    Add args printout to mode menu

--- src/BoosterBot/Program.cs
+++ src/BoosterBot/Program.cs
@@ -145,9 +145,9 @@ internal class Program
                 if (repair)
                     mode = 9;
                 else if (!string.IsNullOrWhiteSpace(gameMode))
-                    mode = gameMode.ToLower() switch { "c" => 1, "conquest" => 1, "l" => 2, "ladder" => 2, "r" => 2, "ranked" => 2, _ => GetModeSelection() };
+                    mode = gameMode.ToLower() switch { "c" => 1, "conquest" => 1, "l" => 2, "ladder" => 2, "r" => 2, "ranked" => 2, _ => GetModeSelection(args) };
                 else
-                    mode = GetModeSelection();
+                    mode = GetModeSelection(args);
 
                 GameState maxTier = GameState.UNKNOWN;
                 if (mode == 1)
@@ -171,7 +171,7 @@ internal class Program
 
                 var retreat = maxTurns > 0 || repair ? maxTurns : GetRetreatAfterTurn();
 
-                PrintTitle();
+                PrintTitle(args);
 
                 var type = (GameMode)mode;
                 var logPath = $"logs\\{type.ToString().ToLower()}-log-{DateTime.Now.ToString("yyyyMMddHHmmss")}.txt";
@@ -225,7 +225,7 @@ internal class Program
         }
     }
 
-    internal static void PrintTitle()
+    internal static void PrintTitle(string[]? args = null)
     {
         var version = Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion;
         version = version.Split('+')[0];
@@ -238,6 +238,12 @@ internal class Program
         Console.WriteLine(title);
         Console.WriteLine("****************************************************");
         Console.WriteLine();
+
+        if (args != null && args.Length > 0)
+        {
+            Console.WriteLine("Arguments: " + string.Join(' ', args));
+            Console.WriteLine();
+        }
     }
 
     private static bool GetAppLanguageSelection()
@@ -313,9 +319,9 @@ internal class Program
         return GetAppLanguageSelection();
     }
 
-    private static int GetModeSelection()
+    private static int GetModeSelection(string[] args)
     {
-        PrintTitle();
+        PrintTitle(args);
 
         // Show update notification if available
         if (_updateAvailable)
@@ -334,7 +340,7 @@ internal class Program
         else if (key.KeyChar == '1' || key.KeyChar == '2' || key.KeyChar == '3')
             return int.Parse(key.KeyChar.ToString());
 
-        return GetModeSelection();
+        return GetModeSelection(args);
     }
 
     private static GameState GetMaxTierSelection()
